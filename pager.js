b25e4a5ba383e9d9cf97e85b6c0526ef5b27858d
crackvine <crackvine@personal>
2021-04-23 07:34:28 +0200

    add more tests

--- pager.js
+++ pager.js
@@ -2,89 +2,117 @@ const config = {
   ackTimeoutMinutes: 15,
 };
 
-const wait = (msDelay) => new Promise((resolve) => setTimeout(resolve, msDelay));
+const sleep = (msDelay) => new Promise((resolve) => setTimeout(resolve, msDelay));
 
-const pagerService = (dependencies) => {
+const pagerService = ({
+  escalationPolicyAdapter, timerAdapter, smsAdapter, emailAdapter, logger,
+}) => {
   const monitoredServices = {};
   const notifiedTargets = new Set();
   const semaphores = new Set();
 
-  const {
-    escalationPolicyAdapter,
-    timerAdapter,
-    smsAdapter,
-    emailAdapter,
-  } = dependencies;
-
   const notify = async (targets) => {
     while (semaphores.has('notify')) {
-      await wait(200);
+      await sleep(50); // eslint-disable-line no-await-in-loop
     }
+
     semaphores.add('notify');
 
-    let notifiedTargetCount = 0;
-    await Promise.all(
+    const notifsSent = await Promise.all(
       targets.map(async (target) => {
-        if (notifiedTargets.has(target.id)) return;
+        if (notifiedTargets.has(target.id)) {
+          return 0;
+        }
 
+        logger.line(`notifying: ${target.target} of type ${target.type}`);
         let notifySuccess = false;
         if (target.type === 'sms') notifySuccess = await smsAdapter.notify(target.target);
         if (target.type === 'email') notifySuccess = await emailAdapter.notify(target.target);
         if (notifySuccess) {
           notifiedTargets.add(target.id);
-          notifiedTargetCount += 1;
+          return 1;
         }
+        return 0;
       }),
     );
 
     semaphores.delete('notify');
+
+    const notifiedTargetCount = notifsSent.reduce((acc, sent) => acc + sent, 0);
     return notifiedTargetCount;
   };
 
   const alert = async (serviceId) => {
+    logger.line(`alert received for service ${serviceId}`);
+
     if (!monitoredServices[serviceId] || monitoredServices[serviceId].healthy) {
+      logger.line(`${serviceId} now unhealthy`);
       monitoredServices[serviceId] = {
         level: 0,
         healthy: false,
         acknowledged: false,
       };
 
-      const targets = escalationPolicyAdapter.getTargets(serviceId, 0);
+      const targets = await escalationPolicyAdapter.getTargets(serviceId, 0);
+
+      logger.line(`notifying targets ${JSON.stringify(targets)} for service ${serviceId}`);
       const notified = await notify(targets);
       if (notified) {
+        logger.line(`setting timer for service ${serviceId}`);
         timerAdapter.setTimer(serviceId, config.ackTimeoutMinutes);
       }
     }
   };
 
   const acknowledge = (targetId, serviceId) => {
+    logger.line(`service ${serviceId} acknowledged by target ${targetId}`);
     if (notifiedTargets.has(targetId)) notifiedTargets.delete(targetId);
 
     if (monitoredServices[serviceId]) {
+      logger.line(`setting acknowledgement for service ${serviceId}`);
       monitoredServices[serviceId].acknowledged = true;
     }
   };
 
   const timeout = async (serviceId) => {
-    if (monitoredServices[serviceId].healthy || monitoredServices[serviceId].acknowledged) return;
+    if (!monitoredServices[serviceId]
+      || monitoredServices[serviceId].healthy
+      || monitoredServices[serviceId].acknowledged) return;
 
+    logger.line(`timeout received for unhealthy service ${serviceId}`);
     const maxLevel = escalationPolicyAdapter.getLevels(serviceId).length;
     if (monitoredServices[serviceId] && monitoredServices[serviceId].level < maxLevel) {
       monitoredServices[serviceId].level += 1;
-      const targets = escalationPolicyAdapter.getTargets(serviceId, monitoredServices[serviceId].level);
+      logger.line(`service ${serviceId} escalated to ${monitoredServices[serviceId].level}`);
+
+      const targets = await escalationPolicyAdapter.getTargets(serviceId, monitoredServices[serviceId].level);
+      logger.line('escalated targets', targets);
       const notified = await notify(targets);
+      logger.line(`notified ${notified} escalated targets`);
+
       if (notified) {
+        logger.line(`Setting timer for service ${serviceId}`);
         timerAdapter.setTimer(serviceId, config.ackTimeoutMinutes);
       }
     }
   };
 
   const clear = (serviceId) => {
-    monitoredServices[serviceId] = {
-      level: 0,
-      healthy: true,
-      acknowledged: false,
-    };
+    if (!monitoredServices[serviceId]) return;
+    logger.line(`clearing status for service ${serviceId}`);
+    monitoredServices[serviceId].level = 0;
+    monitoredServices[serviceId].healthy = true;
+    monitoredServices[serviceId].acknowledged = false;
+  };
+
+  const clearAll = () => {
+    logger.line('clearing all status');
+    for (const id in monitoredServices) {
+      if ({}.hasOwnProperty.call(monitoredServices, id)) {
+        monitoredServices[id] = null;
+      }
+    }
+    notifiedTargets.clear();
   };
 
   const serviceStatus = (serviceId) => (serviceId ? monitoredServices[serviceId] : monitoredServices);
@@ -96,6 +124,7 @@ const pagerService = (dependencies) => {
     acknowledge,
     timeout,
     clear,
+    clearAll,
     serviceStatus,
     notificationStatus,
   };
--- test.js
+++ test.js
@@ -1,30 +1,47 @@
 const pagerService = require('./pager.js');
 
+// Mock dependencies
+const escalationPolicyAdapter = {
+  getTargets: jest.fn((_, level = 0) => {
+    const levels = [
+      { targets: [{ id: 7, type: 'sms', target: '+3456556' }, { id: 3, type: 'email', target: 'fp@work.com' }] },
+      { targets: [{ id: 6, type: 'email', target: 'fp@home.com' }, { id: 9, type: 'sms', target: '+34123232' }] },
+    ];
+    return levels[level].targets;
+  }),
+  getLevels: jest.fn(() => ([0, 1])),
+};
+
+const smsAdapter = {
+  notify: jest.fn(() => true),
+};
+
+const emailAdapter = {
+  notify: jest.fn(() => true),
+};
+
+const timerAdapter = {
+  setTimer: jest.fn(() => true),
+};
+
+const logger = {
+  // line: (line) => console.debug(line), // enable logging
+  line: () => {}, // disable logging
+};
+
+// Instatiate pager service
+const pager = pagerService({
+  escalationPolicyAdapter, smsAdapter, emailAdapter, timerAdapter, logger,
+});
+
 describe('alert tests', () => {
-  const escalationPolicyAdapter = {
-    getTargets: jest.fn(() => ([
-      { id: 7, type: 'sms', target: '+3456556' },
-      { id: 3, type: 'email', target: 'fp@work.com' }]
-    )),
-  };
+  beforeEach(() => {
+    jest.clearAllMocks();
+    pager.clearAll();
+  });
 
-  const smsAdapter = {
-    notify: jest.fn(() => true),
-  };
-
-  const emailAdapter = {
-    notify: jest.fn(() => true),
-  };
-
-  const timerAdapter = {
-    setTimer: jest.fn(() => true),
-  };
-
-  test('alert flags a service as unhealthy', () => {
-    const pager = pagerService({
-      escalationPolicyAdapter, smsAdapter, emailAdapter, timerAdapter,
-    });
-    pager.alert(1000);
+  test('alert flags a service as unhealthy', async () => {
+    await pager.alert(1000);
 
     expect(pager.serviceStatus(1000)).toEqual({
       acknowledged: false,
@@ -33,19 +50,13 @@ describe('alert tests', () => {
     });
   });
 
-  test('alert calls the escalationPolicyAdapter with expected params', () => {
-    const pager = pagerService({
-      escalationPolicyAdapter, smsAdapter, emailAdapter, timerAdapter,
-    });
-    pager.alert(1000);
+  test('alert calls the escalationPolicyAdapter with expected params', async () => {
+    await pager.alert(1000);
 
     expect(escalationPolicyAdapter.getTargets).toHaveBeenCalledWith(1000, 0);
   });
 
   test('alert calls sms and email adapters with expected params', async () => {
-    const pager = pagerService({
-      escalationPolicyAdapter, smsAdapter, emailAdapter, timerAdapter,
-    });
     await pager.alert(1000);
 
     expect(smsAdapter.notify).toHaveBeenCalledWith('+3456556');
@@ -53,25 +64,122 @@ describe('alert tests', () => {
   });
 
   test('alert notifies targets', async () => {
-    const pager = pagerService({
-      escalationPolicyAdapter, smsAdapter, emailAdapter, timerAdapter,
-    });
     await pager.alert(1000);
 
     expect(pager.notificationStatus()).toEqual(new Set([3, 7]));
   });
 
-  test('multiple alerts should not re-notify', () => {
-    const pager = pagerService({
-      escalationPolicyAdapter, smsAdapter, emailAdapter, timerAdapter,
-    });
-    pager.alert(1000);
-    pager.alert(2000);
-    pager.alert(3000);
-    pager.alert(1000);
-    pager.alert(1000);
+  test('multiple alerts should not re-notify the same targets', async () => {
+    await Promise.all([
+      pager.alert(1000),
+      pager.alert(1000),
+      pager.alert(1000),
+      pager.alert(1000),
+      pager.alert(1000),
+    ]);
+    expect(smsAdapter.notify.mock.calls.length).toBe(1);
+    expect(emailAdapter.notify.mock.calls.length).toBe(1);
+  });
+});
 
-    expect(smsAdapter.notify.mock.calls.length).toBe(2);
-    expect(emailAdapter.notify.mock.calls.length).toBe(2);
+describe('acknowledge tests', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    pager.clearAll();
+  });
+
+  test('alert flagged as acknowledged', async () => {
+    await pager.alert(1000);
+    expect(pager.serviceStatus(1000)).toEqual({
+      acknowledged: false,
+      healthy: false,
+      level: 0,
+    });
+
+    pager.acknowledge(7, 1000);
+
+    expect(pager.serviceStatus(1000)).toEqual({
+      acknowledged: true,
+      healthy: false,
+      level: 0,
+    });
+  });
+
+  test('alert not flagged as acknowledged for incorrect serviceId', async () => {
+    await pager.alert(1000);
+
+    expect(pager.serviceStatus(1000)).toEqual({
+      acknowledged: false,
+      healthy: false,
+      level: 0,
+    });
+
+    pager.acknowledge(7, 9999);
+
+    expect(pager.serviceStatus(1000)).toEqual({
+      acknowledged: false,
+      healthy: false,
+      level: 0,
+    });
+  });
+
+  test('acknowledge resets target notified status', async () => {
+    await pager.alert(1000);
+
+    expect(pager.serviceStatus(1000)).toEqual({
+      acknowledged: false,
+      healthy: false,
+      level: 0,
+    });
+
+    expect(pager.notificationStatus()).toEqual(new Set([3, 7]));
+
+    pager.acknowledge(7, 1000);
+
+    expect(pager.notificationStatus()).toEqual(new Set([3]));
+  });
+});
+
+describe('timeout tests', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    pager.clearAll();
+  });
+
+  test('timeout does nothing for a healthy service', async () => {
+    await pager.alert(1000);
+    pager.clear(1000);
+    await pager.timeout(1000);
+
+    expect(pager.serviceStatus(1000)).toEqual({
+      acknowledged: false,
+      healthy: true,
+      level: 0,
+    });
+  });
+
+  test('timeout does not notify targets for an acknowledged alert', async () => {
+    await pager.alert(1000);
+    pager.acknowledge(7, 1000);
+    await pager.timeout(1000);
+
+    expect(smsAdapter.notify.mock.calls.length).toBe(1);
+    expect(emailAdapter.notify.mock.calls.length).toBe(1);
+  });
+
+  test('timeout calls the escalationPolicyAdapter with expected params', async () => {
+    await pager.alert(1000);
+    await pager.timeout(1000);
+
+    expect(escalationPolicyAdapter.getLevels).toHaveBeenCalledWith(1000);
+    expect(escalationPolicyAdapter.getTargets).toHaveBeenCalledWith(1000, 1);
+  });
+
+  test('timeout notifies escalated targets', async () => {
+    await pager.alert(1000);
+    await pager.timeout(1000);
+
+    expect(emailAdapter.notify).toHaveBeenCalledWith('fp@home.com');
+    expect(smsAdapter.notify).toHaveBeenCalledWith('+34123232');
   });
 });
